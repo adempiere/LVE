185c4c3f43173b709902a07155db3b6b5ed20227
Yamel Senih (http://erpya.com/) <ysenih@erpya.com>
2019-01-10 22:13:12 -0430

    Change license to GPL3

--- core/src/main/java/base/org/erpya/lve/model/LVE.java
+++ core/src/main/java/base/org/erpya/lve/model/LVE.java
@@ -1,20 +1,18 @@
-/**************************************************************************************
- * Product: Adempiere ERP & CRM Smart Business Solution                               *
- * This program is free software; you can redistribute it and/or modify it    		  *
- * under the terms version 2 or later of the GNU General Public License as published  *
- * by the Free Software Foundation. This program is distributed in the hope           *
- * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied         *
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                   *
- * See the GNU General Public License for more details.                               *
- * You should have received a copy of the GNU General Public License along            *
- * with this program; if not, printLine to the Free Software Foundation, Inc.,        *
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                             *
- * For the text or an alternative of this public license, you may reach us            *
- * Copyright (C) 2012-2018 E.R.P. Consultores y Asociados, S.A. All Rights Reserved.  *
- * Contributor: Yamel Senih ysenih@erpya.com                                          *
- * Contributor: Carlos Parada cparada@erpya.com                                       *
- * See: www.erpya.com                                                                 *
- *************************************************************************************/
+/*************************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                              *
+ * Copyright (C) 2012-2018 E.R.P. Consultores y Asociados, C.A.                      *
+ * Contributor(s): Yamel Senih ysenih@erpya.com                                      *
+ * This program is free software: you can redistribute it and/or modify              *
+ * it under the terms of the GNU General Public License as published by              *
+ * the Free Software Foundation, either version 3 of the License, or                 *
+ * (at your option) any later version.                                               *
+ * This program is distributed in the hope that it will be useful,                   *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                    *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                     *
+ * GNU General Public License for more details.                                      *
+ * You should have received a copy of the GNU General Public License                 *
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.            *
+ ************************************************************************************/
 package org.erpya.lve.model;
 
 import org.compiere.model.I_C_BPartner;
--- financial/src/main/java/base/org/erpya/lve/process/ProcessPaymentForFBTTAbstract.java
+++ financial/src/main/java/base/org/erpya/lve/process/ProcessPaymentForFBTTAbstract.java
@@ -1,20 +1,18 @@
-/******************************************************************************
- * Product: ADempiere ERP & CRM Smart Business Solution                       *
- * Copyright (C) 2006-2017 ADempiere Foundation, All Rights Reserved.         *
- * This program is free software, you can redistribute it and/or modify it    *
- * under the terms version 2 of the GNU General Public License as published   *
- * or (at your option) any later version.										*
- * by the Free Software Foundation. This program is distributed in the hope   *
- * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
- * See the GNU General Public License for more details.                       *
- * You should have received a copy of the GNU General Public License along    *
- * with this program, if not, write to the Free Software Foundation, Inc.,    *
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
- * For the text or an alternative of this public license, you may reach us    *
- * or via info@adempiere.net or http://www.adempiere.net/license.html         *
- *****************************************************************************/
-
+/*************************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                              *
+ * Copyright (C) 2012-2018 E.R.P. Consultores y Asociados, C.A.                      *
+ * Contributor(s): Yamel Senih ysenih@erpya.com                                      *
+ * This program is free software: you can redistribute it and/or modify              *
+ * it under the terms of the GNU General Public License as published by              *
+ * the Free Software Foundation, either version 3 of the License, or                 *
+ * (at your option) any later version.                                               *
+ * This program is distributed in the hope that it will be useful,                   *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                    *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                     *
+ * GNU General Public License for more details.                                      *
+ * You should have received a copy of the GNU General Public License                 *
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.            *
+ ************************************************************************************/
 package org.erpya.lve.process;
 
 import java.sql.Timestamp;
