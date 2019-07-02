a74a286130eb3f07e732d6b3f2e07c144a0f4836
Yamel Senih <ysenih@erpya.com>
2019-07-01 21:56:56 -0400

    Se agrega soporte a retenciones acumulativas

--- core/src/main/java/base/org/erpya/lve/util/ColumnsAdded.java
+++ core/src/main/java/base/org/erpya/lve/util/ColumnsAdded.java
@@ -68,5 +68,7 @@ public class ColumnsAdded {
 	public static final String COLUMNNAME_VariableRate = "Col_1";
 	/**Withholding Subtrahend*/
 	public static final String COLUMNNAME_Subtrahend = "Subtrahend";
+	/**Withholding Is Cumulative */
+	public static final String COLUMNNAME_IsCumulativeWithholding = "IsCumulativeWithholding";
 	
 }
