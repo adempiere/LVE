a268f5c702b5e79f38a8c44f3e69467ff830df5f
Yamel Senih <ysenih@erpya.com>
2020-07-16 11:48:16 -0400

    Se agrega Definición de Reporte

--- db/ddlutils/postgresql/views/LVE_RV_C_Invoice_Day_Summary.sql
+++ db/ddlutils/postgresql/views/LVE_RV_C_Invoice_Day_Summary.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE VIEW LVE_RV_C_Invoice_Day_Summary
 (AD_Client_ID, AD_Org_ID, DateInvoiced, LineNetAmt, 
  LineListAmt, LineLimitAmt, LineDiscountAmt, LineDiscount, LineOverLimitAmt, 
- LineOverLimit, IsSOTrx, DocStatus, DocNoSequence_ID, ControlNoSequence_ID, IsFiscalDocument, BeginingDocumentNo, EndingDocumentNo, BeginingControlNo, EndingControlNo)
+ LineOverLimit, IsSOTrx, DocStatus, DocNoSequence_ID, ControlNoSequence_ID, IsFiscalDocument, BeginingDocumentNo, EndingDocumentNo, BeginingControlNo, EndingControlNo, DocBaseType)
 AS 
 SELECT il.AD_Client_ID, il.AD_Org_ID,
 	firstOf(il.DateInvoiced, 'DD') AS DateInvoiced,	--	DD Day, DY Week, MM Month
@@ -19,8 +19,9 @@ SELECT il.AD_Client_ID, il.AD_Org_ID,
     MIN(i.DocumentNo) AS BeginingDocumentNo,
     MAX(i.DocumentNo) AS EndingDocumentNo,
     MIN(i.ControlNo) AS BeginingControlNo,
-    MAX(i.ControlNo) AS EndingControlNo
+    MAX(i.ControlNo) AS EndingControlNo,
+    dt.DocBaseType
 FROM RV_C_InvoiceLine il
 INNER JOIN C_Invoice i ON(i.C_Invoice_ID = il.C_Invoice_ID)
 INNER JOIN C_DocType dt ON(dt.C_DocType_ID = il.C_DocTypeTarget_ID)
-GROUP BY il.AD_Client_ID, il.AD_Org_ID, firstOf(il.DateInvoiced, 'DD'), il.IsSOTrx, COALESCE(dt.DefiniteSequence_ID, dt.DocNoSequence_ID), dt.ControlNoSequence_ID, il.DocStatus, i.IsFiscalDocument;
+GROUP BY il.AD_Client_ID, il.AD_Org_ID, firstOf(il.DateInvoiced, 'DD'), il.IsSOTrx, COALESCE(dt.DefiniteSequence_ID, dt.DocNoSequence_ID), dt.ControlNoSequence_ID, il.DocStatus, i.IsFiscalDocument, dt.DocBaseType;
