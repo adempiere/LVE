d73cf8d2897e0587565289c870e67a05f50754da
Raul Muñoz <rmunoz@erpcya.com>
2020-09-18 12:02:49 -0400

    Update HR_BANAVIH.java

--- human_resource/src/main/java/base/org/erpya/lve/util/HR_BANAVIH.java
+++ human_resource/src/main/java/base/org/erpya/lve/util/HR_BANAVIH.java
@@ -98,7 +98,7 @@ public class HR_BANAVIH extends AbstractPayrollReportExport {
 			pathName
 			    .append(PAYROLL_CONSTANT)
 				//	Payroll Account
-				.append(pdl.getAD_Org().get_ValueAsString(ColumnsAdded.COLUMNNAME_BANAVIHCode))
+				.append(MOrg.get(getCtx(), pdl.getAD_Org_ID()).get_ValueAsString(ColumnsAdded.COLUMNNAME_BANAVIHCode))
 				//	Accounting Date in format MM YYYY
 				.append(new SimpleDateFormat("MMyyyy").format(pdl.getDateAcct()));
 			
