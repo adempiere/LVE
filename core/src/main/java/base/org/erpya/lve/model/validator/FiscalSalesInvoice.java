3cdef17622511e177107974c0f76e5a44ac53197
Jesús Albujas <jesusramirez35000@gmail.com>
2026-04-13 11:59:08 -0400

    fix: Update contributor information in FiscalSalesInvoice.java

--- core/src/main/java/base/org/erpya/lve/model/validator/FiscalSalesInvoice.java
+++ core/src/main/java/base/org/erpya/lve/model/validator/FiscalSalesInvoice.java
@@ -1,6 +1,6 @@
 /************************************************************************************
  * Copyright (C) 2012-2026 E.R.P. Consultores y Asociados, C.A.                     *
- * Contributor(s): Yamel Senih ysenih@erpya.com                                     *
+ * Contributor(s): Jesús Albujas, jesusramirez35000@gmail.com                                     *
  * This program is free software: you can redistribute it and/or modify             *
  * it under the terms of the GNU General Public License as published by             *
  * the Free Software Foundation, either version 2 of the License, or                *
